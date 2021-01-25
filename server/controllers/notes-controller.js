cc84457a604809488cf304e768d7e241be57cf9e
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-25 22:05:51 +0100

    removed try catch and fixed all possible scenarios of requests

--- server/controllers/notes-controller.js
+++ server/controllers/notes-controller.js
@@ -1,5 +1,17 @@
 const Note = require("../models/note-model");
 
+const mapToRecent = (note) => {
+  return {
+    title: note.current.title,
+    content: note.current.content,
+    _id: note._id,
+    createdAt: note.createdAt,
+    updatedAt: note.updatedAt,
+    deleted: note.deleted,
+    version: note.version,
+  };
+};
+
 createNote = (req, res) => {
   const body = req.body;
   if (!body) {
@@ -8,38 +20,28 @@ createNote = (req, res) => {
       error: "You need to provide a proper object to create a note",
     });
   }
-  const {
-    title = "",
-    content = "",
-    version = 0,
-    history = [],
-    deleted = false,
-  } = body;
+  const { title = "", content = "" } = body;
   if (!title || !content) {
     return res.status(400).json({
       success: false,
       error: "Missing parameters",
     });
   }
+  const deleted = false;
+  const history = [];
+  const version = 0;
+
   const note = new Note({
     current: { title: title, content: content },
-    version,
-    history,
-    deleted,
+    version: version,
+    history: history,
+    deleted: deleted,
   });
-
-  if (!note) {
-    return res.status(400).json({
-      success: false,
-      error: "There was an issue with creating a note",
-    });
-  }
 
   note
     .save()
     .then((note) => {
       return res.status(201).json({
-        data: note,
         success: true,
         _id: note._id,
         message: "Note added!",
@@ -58,220 +60,184 @@ updateNote = async (req, res) => {
   if (!body) {
     return res.status(400).json({
       success: false,
-      error: "You need to provide a proper object",
+      error: "You need to provide a proper object tu update a note",
     });
   }
   const { title = "", content = "" } = body;
-
   // both parameters are required to update
-
   if (!title || !content) {
     return res.status(400).json({
       success: false,
       error: "Missing update parameters",
     });
   }
-  try {
-    Note.findOne({ _id: req.params.id }, (error, note) => {
-      if (error) {
+  Note.findOne({ _id: req.params.id }, (error, note) => {
+    if (error || !note) {
+      return res.status(404).json({
+        error,
+        message: "There was an error trying to get a note",
+      });
+    }
+    if (!note) {
+      return res.status(404).json({
+        error,
+        message: "Note not found!",
+      });
+    }
+    //pushing an object that we can later use for history
+    note.history.push(
+      JSON.parse(
+        JSON.stringify({
+          note: note.current,
+          updatedAt: note.updatedAt,
+          version: note.version,
+        })
+      )
+    );
+    note.current.title = title;
+    note.current.content = content;
+    note.version = note.version + 1;
+    note
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          _id: note._id,
+          message: "Note updated!",
+        });
+      })
+      .catch((error) => {
         return res.status(404).json({
           error,
-          message: "Note not found!",
+          message: "Note not updated!",
         });
-      }
-      //pushing an object that we can later use for history
-      note.history.push(
-        JSON.parse(
-          JSON.stringify({
-            note: note.current,
-            modified: note.updatedAt,
-            version: note.version,
-          })
-        )
-      );
-      note.current.title = title ? title : note.current.title;
-      note.current.content = content ? content : note.current.content;
-      note.version = note.version + 1;
-      note
-        .save()
-        .then(() => {
-          return res.status(200).json({
-            data: note,
-            success: true,
-            _id: note._id,
-            message: "Note updated!",
-          });
-        })
-        .catch((error) => {
-          return res.status(404).json({
-            error,
-            message: "Note not updated!",
-          });
-        });
-    });
-  } catch (err) {
-    console.log("Something went wrong trying to update a note by id");
-    return res.status(500).json({ success: false });
-  }
+      });
+  });
 };
 
 deleteNote = async (req, res) => {
-  try {
-    Note.findOne({ _id: req.params.id }, (error, note) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          error: "Note not found!",
-        });
-      }
-      if (!note) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Note not found` });
-      }
-      note.deleted = true;
-      note.save().then(() => {
-        return res.status(200).json({
-          note: note,
-          success: true,
-          id: note._id,
-          message: "Note marked as deleted!",
-        });
+  Note.findOne({ _id: req.params.id }, (error, note) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        error: "There was an error trying to get the note",
       });
-      return res.status(200).json({ success: true, data: note });
+    }
+    if (!note) {
+      return res.status(404).json({ success: false, error: `Note not found` });
+    }
+    note.deleted = true;
+    note.save().then(() => {
+      return res.status(200).json({
+        success: true,
+        id: note._id,
+        message: "Note marked as deleted!",
+      });
     });
-  } catch (err) {
-    console.log("Sometging went wrong trying to delete a note by id");
-    return res.status(500).json({ success: false });
-  }
+  });
 };
 
 getNoteById = async (req, res) => {
-  try {
-    Note.findOne({ _id: req.params.id, deleted: false }, (error, note) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          error: "There was an error getting the note",
-        });
-      }
-
-      if (!note) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Note not found` });
-      }
-      return res.status(200).json({ success: true, data: note });
-    });
-  } catch (err) {
-    console.log("Something went wrong trying to get a not deleted note by id");
-    return res.status(500).json({ success: false });
-  }
+  Note.findOne({ _id: req.params.id, deleted: false }, (error, note) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        error: "There was an error trying to get the note",
+      });
+    }
+    if (!note) {
+      return res.status(404).json({ success: false, error: `Note not found` });
+    }
+    return res.status(200).json({ success: true, data: mapToRecent(note) });
+  });
 };
 
 getNotes = async (req, res) => {
-  try {
-    Note.find({ deleted: false }, (error, notes) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          error: "There was an error getting the notes",
-        });
-      }
-      if (!notes.length) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Notes not found` });
-      }
-      return res.status(200).json({ success: true, data: notes });
-    });
-  } catch (err) {
-    console.log("Something went wrong trying to get all not deleted notes");
-    return res.status(500).json({ success: false });
-  }
+  Note.find({ deleted: false }, (error, notes) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        error: "There was an error trying to get the notes",
+      });
+    }
+    if (!notes.length) {
+      return res.status(404).json({ success: false, error: `Notes not found` });
+    }
+    const recentNotes = notes.map((note) => mapToRecent(note));
+    return res.status(200).json({ success: true, data: recentNotes });
+  });
 };
 getDeletedNotes = async (req, res) => {
-  try {
-    Note.find({ deleted: true }, (error, notes) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          error: "There was an error getting the notes",
-        });
-      }
-      if (!notes.length) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Notes not found` });
-      }
-      return res.status(200).json({ success: true, data: notes });
-    });
-  } catch (err) {
-    console.log("Something went wrong trying to get deleted notes");
-    return res.status(500).json({ success: false });
-  }
-};
-getAnyNoteById = async (req, res) => {
-  try {
-    Note.findOne({ _id: req.params.id }, (error, note) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          error: "There was an error getting the note",
-        });
-      }
-      if (!note) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Note not found` });
-      }
-      return res.status(200).json({ success: true, data: note });
-    });
-  } catch (err) {
-    console.log("Something went wrong trying to get eny note by id");
-    return res.status(500).json({ success: false });
-  }
-};
-testSetup = async (req, res) => {
-  try {
-    Note.find({}, (error, notes) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          error: "There was an error getting the notes",
-        });
-      }
-      const testNotes = [
-        {
-          title: "Title - not deleted",
-          content: "Note content",
-          deleted: false,
-        },
-        { title: "Title - deleted", content: "Note content", deleted: true },
-      ];
-      if (notes) {
-        notes.forEach((note) => {
-          note.delete();
-        });
-      }
-      testNotes.forEach((note) => {
-        new Note({
-          current: { title: note.title, content: note.content },
-          deleted: note.deleted,
-          history: [],
-          version: 0,
-        }).save();
+  Note.find({ deleted: true }, (error, notes) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        error: "There was an error trying to get the notes",
       });
-      return res.status(200).json({ success: true });
+    }
+    if (!notes.length) {
+      return res.status(404).json({ success: false, error: `Notes not found` });
+    }
+    const recentNotes = notes.map((note) => mapToRecent(note));
+    return res.status(200).json({ success: true, data: recentNotes });
+  });
+};
+getFullNoteById = async (req, res) => {
+  Note.findOne({ _id: req.params.id }, (error, note) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        error: "There was an error trying to get the note",
+      });
+    }
+    if (!note) {
+      return res.status(404).json({ success: false, error: `Note not found` });
+    }
+    return res.status(200).json({ success: true, data: note });
+  });
+};
+
+// endpoint to prepare the database to test CRUD operations
+
+testSetup = async (req, res) => {
+  Note.find({}, (error, notes) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        error: "There was an error trying to get the notes",
+      });
+    }
+    if (notes) {
+      notes.forEach((note) => {
+        note.delete();
+      });
+    }
+    const testNotes = [
+      {
+        title: "Title of an example note",
+        content: "Example content",
+        deleted: false,
+      },
+      {
+        title: "Title of a deleted note",
+        content: "Deleted note content",
+        deleted: true,
+      },
+    ];
+    testNotes.forEach((note) => {
+      new Note({
+        current: { title: note.title, content: note.content },
+        deleted: note.deleted,
+        history: [],
+        version: 0,
+      }).save();
     });
-  } catch (err) {
-    console.log("Something went wrong setting up the database for tests");
-    return res.status(500).json({ success: false });
-  }
+    return res.status(200).json({ success: true });
+  });
 };
 
 module.exports = {
-  getAnyNoteById,
+  getFullNoteById,
   getDeletedNotes,
   createNote,
   updateNote,
