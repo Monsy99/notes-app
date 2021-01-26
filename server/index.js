7286a2111c1717b6f29339437de856f9c59a633f
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-26 11:34:53 +0100

    renamed router file to "api-router" and removed handling "/" get requests

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const db = require("./db");
-const notesRouter = require("./routes/notes-router");
+const router = require("./routes/api-router");
 
 const app = express();
 const apiPort = 3000;
@@ -11,11 +10,7 @@ app.use(express.json());
 
 db.on("error", console.error.bind(console, "MongoDB connection error"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.use("/api", notesRouter);
+app.use("/api", router);
 
 //export for testing
 module.exports = app.listen(apiPort, () =>
