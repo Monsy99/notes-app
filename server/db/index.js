126ec3cf0e73facc6cf2336f8f6a9c272ba90e5b
Szymon Marczuk <szymon.marczukk@gmail.com>
2021-01-26 11:32:58 +0100

    removed conditional mongourl from db connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const url = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/notes";
+const url = "mongodb://127.0.0.1:27017/notes";
 mongoose
   .connect(url, {
     useNewUrlParser: true,
